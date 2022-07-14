d8ff2a52877769aeb7d029c725ed605e6307ac77
Gautier Marechal <gaut.marechal@gmail.com>
2022-07-14 18:42:15 +0200

    Progress ex 2

---
+++ exercise-2/index.js
@@ -0,0 +1,39 @@
+const config = require("./configuration.json");
+const changes = require("./changes.json");
+const fs = require("fs");
+
+function main(changesParam) {
+  const unparsedChangesFieldsList = Object.keys(changesParam);
+  const fieldsToChange = unparsedChangesFieldsList
+    .map((field) => field.split(/\[([0-9]+)\]/))
+    .map((line) => line.filter((key) => key !== ""));
+  const setNestedKey = (obj, path, value) => {
+    if (path.length === 1) {
+      if (isNaN(parseInt(path))) {
+        obj[path] = value;
+      } else {
+        console.log(obj);
+        console.log(parseInt(path));
+        obj[parseInt(path)] = value;
+      }
+      return obj;
+    }
+    return setNestedKey(obj[path[0]], path.slice(1), value);
+  };
+
+  fieldsToChange.forEach((item, index) => {
+    return setNestedKey(
+      config,
+      item,
+      changesParam[unparsedChangesFieldsList[index]]
+    );
+  });
+  const jsonResult = JSON.stringify(config);
+
+  // fs.writeFile("configuration.json", jsonResult, "utf8", function (err) {
+  //   if (err) throw err;
+  //   console.log("File successfully updated");
+  // });
+}
+
+main(changes);
